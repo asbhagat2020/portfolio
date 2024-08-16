15c711ba91f15a3d29937e630d07209b2af86cce
Ashwin <asbhagat2020@gmail.com>
2024-08-16 13:26:00 +0530

    some part add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
-                   
+            
             </Nav.Item>
 
             <Nav.Item className="fork-btn">
